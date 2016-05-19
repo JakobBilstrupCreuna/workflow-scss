7d683c70a45bec424789fa87648d1686d614db72
Jakob Bilstrup <jakob@kahlogbilstrup.dk>
2016-05-19 09:17:50 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+// Runs compiler for SCSS with autoprefixer
+gulp.task('styles', function () {
+  return gulp.src('scss/**/*.scss')
+		.pipe(autoprefixer())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('stylesheets/'));
+});
+
+
+// Opens a new browser that sync on changes
+gulp.task('browser-sync', function() {
+   browserSync.init({
+       server: {
+           baseDir: "./"
+       }
+   });
+});
+
+// Watching changes in .html and .scss
+gulp.task('watch', function () {
+	gulp.watch('scss/**/*.scss', ['styles']).on('change', browserSync.reload);
+	gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['watch', 'browser-sync']);

ce1aa6924696d0a093251a85e3bab7817f340e3b
Jakob Bilstrup <jakob@kahlogbilstrup.dk>
2016-05-23 10:12:51 +0200

    added flip function
    + some base styling on the backside preparing for Daniel.

--- js/min/script.js
+++ js/min/script.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){console.log("Workflow-SCSS!"),$(".btn-ingredienser").click(function(){$(this).parents(".card-item-grid").toggleClass("flip")})});
+$(document).ready(function(){console.log("Workflow-SCSS!"),$(".btn-ingredienser").click(function(){$(this).parents(".card-item-grid").toggleClass("flipped")})});
--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     console.log( "Workflow-SCSS!" );
 
   $(".btn-ingredienser").click(function(){
-    $(this).parents('.card-item-grid').toggleClass("flip");
+    $(this).parents('.card-item-grid').toggleClass("flipped");
   });
 
 });

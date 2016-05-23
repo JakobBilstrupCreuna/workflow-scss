b3150e424cc74794dd84c353e99e5f5f2d6c8adc
Daniel Malmros <daniel@bymalmros.dk>
2016-05-23 13:56:34 +0200

    work on back card

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-$( document ).ready(function() {
-    console.log( "Workflow-SCSS!" );
+$(document).ready(function() {
+    console.log("Workflow-SCSS!");
 
-  $(".btn-ingredienser").click(function(){
-    $(this).parents('.card-item-grid').toggleClass("flipped");
-  });
+    $(".btn-ingredienser").click(function() {
+        $(this).parents('.card-item-grid').toggleClass("flipped");
+    });
+
+    // Ingredien list order script
+    // Variabler til listen!
+    var list = $(".card-item__ingrediens-list"),
+        itemOrder = list.children();
+
+    list.on("click", ":checkbox", function() {
+        var i, checked = document.createDocumentFragment(),
+            unchecked = document.createDocumentFragment();
+
+        // i skifter position af itemOrder
+        for (i = 0; i < itemOrder.length; i++) {
+            if (itemOrder[i].getElementsByTagName("input")[0].checked) {
+                checked.appendChild(itemOrder[i]);
+            } else {
+                unchecked.appendChild(itemOrder[i]);
+            }
+        }
+        list.append(checked).append(unchecked);
+    });
 
 });

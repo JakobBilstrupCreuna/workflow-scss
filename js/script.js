a7575cc6e645cf001ec7972c55797674eb1223ab
Jakob Bilstrup <jakob@kahlogbilstrup.dk>
2016-05-23 09:36:52 +0200

    added backside in markup & started on progressbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $( document ).ready(function() {
     console.log( "Workflow-SCSS!" );
+    var bar = new ProgressBar.Line(container, {
+      strokeWidth: 4,
+      easing: 'easeInOut',
+      duration: 1400,
+      color: '#FFEA82',
+      trailColor: '#eee',
+      trailWidth: 1,
+      svgStyle: {width: '100%', height: '100%'}
+    });
+
+    bar.animate(1.0);  // Number from 0.0 to 1.0
 });
